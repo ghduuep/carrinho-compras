339a74db774ceebf6629d34726c758e902d951ed
Guilherme Lima <ghduep@outlook.com>
2024-02-19 10:44:18 -0300

    retirar comentarios

--- js/app.js
+++ js/app.js
@@ -5,23 +5,23 @@ function adicionar() {
     console.log(partesProduto);
     let quantidade = document.getElementById('quantidade').value;
 
-    //Cria o novo produto e o adiciona no carrinho
+
     let carrinho = document.querySelector("#lista-produtos");
     let novoProduto = document.createElement('section');
     novoProduto.classList.add('carrinho__produtos__produto');
     novoProduto.innerHTML = `<span class="texto-azul">${quantidade}x</span> ${partesProduto[0]} <span class="texto-azul">${partesProduto[1]}</span>`;
     carrinho.appendChild(novoProduto);
 
-    //pega o total ja existente no carrinho
+
     let totalExistente = parseInt(pegarTotalExistente());
 
-    //pega os adicionados
+
     let adicionados = parseInt(quantidade * extrairNumero(partesProduto[1]));
 
-    //somar o total existente + adicionados
+
     let totalDaSoma = totalExistente + adicionados;
 
-    //mostrar o total na tela
+
     let mensagemTotal = document.getElementById('valor-total');
     mensagemTotal.innerHTML = `<span class="texto-azul" id="valor-total">R$${totalDaSoma}</span>`
 }
@@ -42,7 +42,7 @@ function limpar() {
     listaDeProdutos.innerHTML = '';
 
 
-    //apagando a mensagem de total
+
     let mensagemTotal = document.getElementById('valor-total')
     mensagemTotal.innerHTML = '<span class="texto-azul" id="valor-total">R$0</span>'
 }
